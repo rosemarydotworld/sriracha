2cd15128cfd0c8ac404ebc0f0bb66e7c570ca88a
Liam Campbell <liam.campbell@moovweb.com>
2013-08-22 15:27:50 -0700

    Converted documentation to almost-Tomdoc

--- sriracha.ts
+++ sriracha.ts
@@ -1,8 +1,16 @@
-# ***
-# Hide either the current scope or a specified scope
+# XMLNode: Hides either the current scope or a specified scope.
 #
-# %xpath (optional) - xpath string matching nodes to be hidden
-# ***
+# %xpath - optional xpath string matching nodes to be hidden.
+#
+# Examples
+#
+#   $(".//div[@id='to-be-hidden']") {
+#     hide()
+#   }
+#
+#   hide(".//div[@id='to-be-hidden']")
+#
+# Yields scope of hidden element.
 @func XMLNode.hide(Text %xpath) {
   match_not(%xpath, /^.+$/) {
     add_class("mw-hide")
@@ -16,11 +24,18 @@
   yield()
 }
 
-# ***
-# Execute arbitrary code if xpath matches no nodes
+# XMLNode: Execute arbitrary code if xpath matches no nodes.
 #
-# %xpath - xpath string to attempt to match
-# ***
+# %xpath - xpath string to attempt to match.
+#
+# Examples
+#
+#   unless(".//div[@id='nonexistent']") {
+#     log("#nonexistent not found!")
+#   }
+#
+# Yields scope in which unless() was run.
+}
 @func XMLNode.unless(Text %xpath) {
   $("self::*[not(" + %xpath + ")]") {
     yield()
